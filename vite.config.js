e1783285fa7e7c51aea78923422ac4b0277b1e5a
TITCLAIRE <thierry.thomas@icloud.com>
2025-08-01 12:11:48 +0200

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'react': 'react',
+      'react-dom': 'react-dom'
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
